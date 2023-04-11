a46c9fe4f8a1edd00a0354cb442a3070c8ca70d6
Kajal Pourjalil <karastar771@gmail.com>
2023-04-11 14:52:16 +0300

    create html and js file

---
+++ script.js
@@ -0,0 +1,24 @@
+const inventors = [
+    { first: 'Meryl', last: 'Streep', age: 1949, passed: 'Summit, NJ'},
+    { first: 'Viola', last: 'Davis', age: 1965, passed: 'St. Matthewes, SC'},
+    { first: 'Scarlett', last: 'Johansson', age: 1984, passed: 'New York, NY'},
+    { first: 'Lupita', last: 'Nyongo', age: 1983, passed: 'Mexico City, Mexico'},
+    { first: 'Emma', last: 'Stone', age: 1988, passed: 'Scottsdale, AZ'},
+    { first: 'Gal', last: 'Gadot', age: 1985, passed: 'Rosh HaAyin, Israel'},
+    { first: 'Jennifer', last: 'Lawrence', age: 1990, passed: 'Indian Hills, KY'},
+    { first: 'Saoirse', last: 'Ronan', age: 1994, passed: 'New York, NY'},
+    { first: 'Brie', last: 'Larson', age: 1989, passed: 'Sacramento, CA'},
+    { first: 'Angelina', last: 'Jolie', age: 1975, passed: 'Los Angeles, CA'},
+    { first: 'Charlize', last: 'Theron', age: 1975, passed: 'Benoni, South Africa'},
+    { first: 'Margot', last: 'Robbie', age: 1990, passed: 'Dalby, Australia'}
+]
+
+
+const people = [
+    'Bernhard, Sandra', 'Bethea, Erin', 'Becker, Carl', 'Bentsen, Lloyd', 'Beckett, Samuel', 'Blake, William', 'Berger, Ric', 'Beddoes, Mick', 'Beethoven, Ludwig',
+    'Belloc, Hilaire', 'Begin, Menachem', 'Bellow, Saul', 'Benchley, Robert', 'Blair, Robert', 'Benenson, Peter', 'Benjamin, Walter', 'Berlin, Irving',
+    'Benn, Tony', 'Benson, Leana', 'Bent, Silas', 'Berle, Milton', 'Berry, Halle', 'Biko, Steve', 'Beck, Glenn', 'Bergman, Ingmar', 'Black, Elk', 'Berio, Luciano',
+    'Berne, Eric', 'Berra, Yogi', 'Berry, Wendell', 'Bevan, Aneurin', 'Ben-Gurion, David', 'Bevel, Ken', 'Biden, Joseph', 'Bennington, Chester', 'Bierce, Ambrose',
+    'Billings, Josh', 'Birrell, Augustine', 'Blair, Tony', 'Beecher, Henry', 'Biondo, Frank'
+];
+
